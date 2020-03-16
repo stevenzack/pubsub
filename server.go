1cbdcd1e6d77638b6e67507b8b9a7cd492501781
stevenzack <zigzigcheers@gmail.com>
2020-03-16 08:24:36 +0800

    syncmap

--- server.go
+++ server.go
@@ -3,11 +3,12 @@ package pubsub
 import (
 	"errors"
 	"fmt"
+	"sync"
 )
 
 type Server struct {
 	log          bool
-	channelMap   map[string]clientMap
+	channelMap   sync.Map //string to *ClientMap
 	entering     chan *Client
 	leaving      chan *Client
 	messaging    chan *message
@@ -21,11 +22,10 @@ func (s *Server) SetMaxClientNum(i int) {
 
 func NewServer() *Server {
 	return &Server{
-		channelMap: make(map[string]clientMap),
-		entering:   make(chan *Client, 24),
-		leaving:    make(chan *Client, 24),
-		messaging:  make(chan *message, 24),
-		shutdown:   make(chan bool, 24),
+		entering:  make(chan *Client, 24),
+		leaving:   make(chan *Client, 24),
+		messaging: make(chan *message, 24),
+		shutdown:  make(chan bool, 24),
 	}
 }
 
@@ -39,6 +39,25 @@ func (s *Server) SetLog(b bool) {
 	s.log = b
 }
 
+func (s *Server) load(chanID string) (*clientMap, bool) {
+	cmv, ok := s.channelMap.Load(chanID)
+	if !ok {
+		return nil, false
+	}
+	return cmv.(*clientMap), true
+}
+
+func (s *Server) rangeChannelMap(fn func(string, *clientMap) bool) {
+	s.channelMap.Range(func(k, v interface{}) bool {
+		return fn(k.(string), v.(*clientMap))
+	})
+}
+
+func (s *Server) loadOrCreate(chanID string) *clientMap {
+	cmv, _ := s.channelMap.LoadOrStore(chanID, newClientMap())
+	return cmv.(*clientMap)
+}
+
 func (s *Server) Run() {
 	for {
 		select {
@@ -46,37 +65,37 @@ func (s *Server) Run() {
 			if s.log {
 				fmt.Println("entering:")
 			}
-			if cm, ok := s.channelMap[cli.channelID]; !ok || cm == nil {
-				s.channelMap[cli.channelID] = newClientMap()
-			}
+
+			cm := s.loadOrCreate(cli.channelID)
 			if s.maxClientNum > 0 {
-				if len(s.channelMap[cli.channelID]) >= s.maxClientNum {
+				if cm.Len() >= s.maxClientNum {
 					pop := s.pop(cli.channelID)
 					if pop != nil {
 						pop.closeChannel()
 					}
 				}
 			}
-			s.channelMap[cli.channelID][cli.id] = cli
+			cm.Store(cli.id, cli)
 		case msg := <-s.messaging:
 			if s.log {
 				fmt.Println("messaging:", msg.data)
 			}
-			if clientmap, ok := s.channelMap[msg.channelID]; ok && clientmap != nil {
-				for _, cli := range clientmap {
+			if cm, ok := s.load(msg.channelID); ok && cm != nil {
+				cm.Range(func(id int64, cli *Client) bool {
 					select {
 					case cli.receiver <- msg.data:
 					default:
 						fmt.Println("pubsub error: client not running")
 					}
-				}
+					return true
+				})
 			}
 		case cli := <-s.leaving:
 			if s.log {
 				fmt.Println("leaving:")
 			}
-			if cm, ok := s.channelMap[cli.channelID]; ok && cm != nil {
-				delete(s.channelMap[cli.channelID], cli.id)
+			if cm, ok := s.load(cli.channelID); ok && cm != nil {
+				cm.Delete(cli.id)
 				cli.closeChannel()
 			}
 		case b := <-s.shutdown:
@@ -84,14 +103,15 @@ func (s *Server) Run() {
 				if s.log {
 					fmt.Println("shutdown:")
 				}
-				for _, cm := range s.channelMap {
+				s.rangeChannelMap(func(_ string, cm *clientMap) bool {
 					if cm != nil {
-						for _, cli := range cm {
+						cm.Range(func(_ int64, cli *Client) bool {
 							cli.closeChannel()
-						}
+							return true
+						})
 					}
-				}
-				break
+					return true
+				})
 			}
 		}
 	}
@@ -114,21 +134,16 @@ func (s *Server) Stop() error {
 	}
 }
 
-func (s *Server) GetClientNum(chanID string) int {
-	return len(s.channelMap[chanID])
-}
-
 func (s *Server) pop(chanID string) *Client {
-	cm, ok := s.channelMap[chanID]
+	cm, ok := s.load(chanID)
 	if !ok {
 		return nil
 	}
-	if len(cm) == 0 {
-		return nil
-	}
-	for k, cli := range cm {
-		delete(s.channelMap[chanID], k)
-		return cli
-	}
-	return nil
+	var cli *Client
+	cm.Range(func(id int64, c *Client) bool {
+		cli = c
+		cm.Delete(id)
+		return false
+	})
+	return cli
 }
